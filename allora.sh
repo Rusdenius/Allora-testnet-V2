1fa59465266019186fb0530c227f91f875d64430
rusdenius <rusdenius@gmail.com>
2024-07-19 00:18:30 +0800

    Update allora.sh

--- allora.sh
+++ allora.sh
@@ -259,7 +259,7 @@ services:
         allora-node --role=worker --peer-db=/data/peerdb --function-db=/data/function-db \
           --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
           --private-key=/data/keys/priv.bin --log-level=debug --port=9011 \
-          --boot-nodes/ip4/172.22.0.100/tcp/9010/p2p/$HEAD_ID \
+          --boot-nodes/ip4/172.22.0.100/tcp/9010/p2p/$head_id \
           --topic=allora-topic-1-worker \
           --allora-chain-key-name=testkey \
           --allora-chain-restore-mnemonic='$seed_phrase' \
